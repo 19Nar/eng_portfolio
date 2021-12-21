77ce2a266458f05423986d6df3a0dd689f771650
narine <narinemkn@gmail.com>
2021-12-21 22:24:04 +0100

    Updates

---
+++ precache-manifest.d8bd13fbf21817912c199b0e7218611e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75573930903dfb7e79ce65c9bbd3aa02",
+    "url": "/eng_portfolio/index.html"
+  },
+  {
+    "revision": "7db9f3a8edd879febd17",
+    "url": "/eng_portfolio/static/css/main.d2eaa0f7.chunk.css"
+  },
+  {
+    "revision": "4bc2587b71b4351c387b",
+    "url": "/eng_portfolio/static/js/2.53d9d743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eng_portfolio/static/js/2.53d9d743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db9f3a8edd879febd17",
+    "url": "/eng_portfolio/static/js/main.090a0c77.chunk.js"
+  },
+  {
+    "revision": "72bf992a03c66e91f21b",
+    "url": "/eng_portfolio/static/js/runtime-main.ed324383.js"
+  },
+  {
+    "revision": "49c0868245fb7c22655471a978708fd7",
+    "url": "/eng_portfolio/static/media/Agile.49c08682.JPG"
+  },
+  {
+    "revision": "79fcdee1cddb8d93b2a477e491391da1",
+    "url": "/eng_portfolio/static/media/BusinessOperations.79fcdee1.JPG"
+  },
+  {
+    "revision": "8d91835360804b1daa16efce88a83589",
+    "url": "/eng_portfolio/static/media/GITHUB.8d918353.png"
+  },
+  {
+    "revision": "47c3bdcd683c7c663ed6ba037e3a2afb",
+    "url": "/eng_portfolio/static/media/PM.47c3bdcd.JPG"
+  },
+  {
+    "revision": "f3c65a2cd8165cf4935d66a95c664b61",
+    "url": "/eng_portfolio/static/media/PRJcollage.f3c65a2c.jpg"
+  },
+  {
+    "revision": "80dc76174172613da9ac7c2ff646fda9",
+    "url": "/eng_portfolio/static/media/book.80dc7617.png"
+  },
+  {
+    "revision": "56c736d8283b1d464543e0525a9f958c",
+    "url": "/eng_portfolio/static/media/credly.56c736d8.JPG"
+  },
+  {
+    "revision": "519034148122a3b6263d6a1f8eedfb41",
+    "url": "/eng_portfolio/static/media/facebook.51903414.png"
+  },
+  {
+    "revision": "2ce8cf695fe47c09d3810b6b2ab4adde",
+    "url": "/eng_portfolio/static/media/ibm.2ce8cf69.png"
+  },
+  {
+    "revision": "377c4ca7896eb08695f8e94bf53bdd85",
+    "url": "/eng_portfolio/static/media/linkedin.377c4ca7.jpg"
+  },
+  {
+    "revision": "dd83a69afe83e108c1c46a80e3c055ae",
+    "url": "/eng_portfolio/static/media/mainframe.dd83a69a.JPG"
+  },
+  {
+    "revision": "addb3122b834566fcefd386d7bbd9ee7",
+    "url": "/eng_portfolio/static/media/movie.addb3122.png"
+  },
+  {
+    "revision": "8f68715e0f8e1ccb2282a2406288b22a",
+    "url": "/eng_portfolio/static/media/weather.8f68715e.png"
+  }
+]);
